f6eaa0b8e0a6ed25bc355178931a51e28f9b7830
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-08-06 20:19:44 -0300

    feat / all estructure

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import signRouter from "./sign.router.js";
-import urlRouters from "./urls.router.js";
+import urlRouter from "./urls.router.js";
 
 const router = Router();
 
 router.use(signRouter);
-router.use(urlRouters);
+router.use(urlRouter);
 
 export default router;
