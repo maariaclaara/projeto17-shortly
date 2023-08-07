0788ca8d4a2a87449326f66bf7f0be7c5b2d2baa
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-08-07 11:44:41 -0300

    fix / adjustments in URL router

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -26,41 +26,38 @@ export async function postUrl (req, res){
 
 export async function getUrl (req, res){
     
-    const {id} = req.params
-    const {shortUrl, url} = req.body
+    const { id } = req.params;
 
-    try{
-        const listUrl = (`SELECT * FROM urls WHERE id = $1, "shortUrl" = $2, url = $3`, 
-        [id, shortUrl, url]);
-
-        if (listUrl.rowCount < 1) {
-            return res.sendStatus(404)
-          };
-
-        res.status(200).send({ id, shortUrl, url });
-    } 
-    catch{
-        res.status(500).send(error.message);
+  try {
+    const { rows, rowCount } = await db.query(`SELECT * FROM urls WHERE id = $1, "shortUrl" = $2, url = $3`, 
+        [id]);
+    if (!rowCount) {
+      return res.sendStatus(404);
     }
+    res.status(200).send(rows[0]);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 
 export async function deleteUrl (req, res){
 
-     const {id} = req.params
-    const {shortUrl, userId} = req.body
+    const {id} = req.params
+    const { userId } = res.locals.session;
 
     try {
-        const deleteUrls = (`SELECT * FROM urls WHERE id = $1, "shortUrl" = $2, "userId" = $3`, 
-        [id, shortUrl, userId]);
-  
-      if (deleteUrls.rowCount < 1) {
-        return res.sendStatus(404)
-      };
-  
-      if (shortUrl.rows[0] != userId.rows[0]) {
-        return res.sendStatus(401)
-      };
+        const { rowCount: urlExists } = await db.query(`SELECT * FROM urls WHERE id = $1, "shortUrl" = $2, url = $3`, 
+        [id]);
+        if (!urlExists) {
+          return res.sendStatus(404);
+        }
+       
+        const { rowCount } = await db.query(`SELECT * FROM urls WHERE id = $1, "shortUrl" = $2, url = $3`, 
+        [id, userId]);
+        if (!rowCount) {
+        return res.sendStatus(401);
+        }
   
       await db.query(`DELETE FROM urls WHERE id=$1`,
         [id]);
@@ -69,6 +66,5 @@ export async function deleteUrl (req, res){
     }
     catch{
       return res.status(500).send(err.message)
-    }
-  
-  }
+    }  
+  };
